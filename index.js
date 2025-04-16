1ef06f4049bcdc4f62f09e039bc4b83835f41293
mpamop-88 <chan-88@ukr.net>
2025-04-16 07:49:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 import 'dotenv/config';
 import { fetchAndSaveBattles } from './fetchBattles.js';
 import { parseBattles } from './parseBattles.js';
+import { getNextBattle, getTimeDiffString } from './utils.js';
 
 async function run() {
   await fetchAndSaveBattles();
   const battles = parseBattles();
-  console.log('🛡 Список боёв:');
-  console.log(battles);
+  const next = getNextBattle(battles);
+
+  if (next) {
+    const diff = getTimeDiffString(next.date);
+    console.log(`🔔 Следующий бой против ${next.enemy} через ${diff}`);
+  } else {
+    console.log('❌ Ближайших боёв не найдено.');
+  }
 }
 
 run();
