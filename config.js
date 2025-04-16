144c97df3ff483c42a8bd87d27949aaa8a5611ba
mpamop-88 <chan-88@ukr.net>
2025-04-16 06:45:24 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+// config.js
+export const config = {
+  BOT_TOKEN: process.env.BOT_TOKEN,
+  LOGIN: process.env.LOGIN,
+  PASSWORD: process.env.PASSWORD,
+  BATTLE_URL: 'https://www.gwars.io/object.php?id=69403&page=oncoming1&sid=9760',
+  BATTLES_FILE: 'battles.html',
+};
