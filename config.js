21753355032b0ef64a0aba3efe292e908c71d45b
mpamop-88 <chan-88@ukr.net>
2025-04-19 12:04:10 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // config.js
 export const config = {
   BOT_TOKEN: process.env.BOT_TOKEN,
-  LOGIN: process.env.LOGIN,
-  PASSWORD: process.env.PASSWORD,
+  login: process.env.LOGIN,
+  pass: process.env.PASSWORD,
   BATTLE_URL: 'https://www.gwars.io/object.php?id=69403&page=oncoming1&sid=9760',
   BATTLES_FILE: 'battles.html',
   CHAT_ID: process.env.CHAT_ID // id группы или канала для уведомлений
